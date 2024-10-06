1ac421603dfa28baad2444f92a69cc668b91464a
gio10herrera <herrerafontalvo@outlook.com>
2024-10-06 18:52:40 -0500

    JScrollPane añadido a JFrame correctamente

--- src/views/JFrameActividad.java
+++ src/views/JFrameActividad.java
@@ -1,6 +1,7 @@
 package views;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class JFrameActividad {
 
@@ -8,6 +9,7 @@ public class JFrameActividad {
         //creacion del JFrame
         JFrame jFrameInterfaz = new JFrame();
         jFrameInterfaz.setSize(400, 400);
+        //jFrameInterfaz.pack();
         jFrameInterfaz.setLocationRelativeTo(null);
         jFrameInterfaz.setVisible(true);
         jFrameInterfaz.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -16,6 +18,15 @@ public class JFrameActividad {
         //creacion de lista de elementos (JList)
         String[] opciones = {"opcion1", "opcion2", "opcion3", "opcion4"};
         JList<String> lista = new JList<>(opciones);
+
+        //añadir la lista dentro del JScrollPane
+        JScrollPane jScrollPane = new JScrollPane(lista);
+        jScrollPane.setBounds(10, 10, 100, 60);//posicionar el jscrollPane
+
+        //añadir jscrollpane dentro del jframe
+        jFrameInterfaz.add(jScrollPane);
+        jFrameInterfaz.setLayout(null);
+
 
     }
 }
