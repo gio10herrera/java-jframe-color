4c8c45598f3cb7e2ca7ef84d653613af39d5ad73
gio10herrera <herrerafontalvo@outlook.com>
2024-10-06 16:59:12 -0500

    creacion de la clase JFrameInterfaz

---
+++ src/views/JFrameInterfaz.java
@@ -0,0 +1,13 @@
+package views;
+
+import javax.swing.*;
+
+public class JFrameInterfaz extends JFrame {
+    public JFrameInterfaz() {
+
+    }
+
+    public static void main(String[] args) {
+        new JFrameInterfaz();
+    }
+}
