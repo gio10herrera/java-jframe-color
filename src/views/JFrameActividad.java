3ca64426d30399863e76308f9ea97f0c17620086
gio10herrera <herrerafontalvo@outlook.com>
2024-10-06 19:35:18 -0500

    cambio de color backg terminado, programa terminado

--- src/views/JFrameActividad.java
+++ src/views/JFrameActividad.java
@@ -6,16 +6,18 @@ import javax.swing.event.ListSelectionListener;
 import java.awt.*;
 
 public class JFrameActividad {
+    static JFrame jFrameInterfaz;
 
     public static void main(String[] args) {
         //creacion del JFrame
-        JFrame jFrameInterfaz = new JFrame();
+        jFrameInterfaz = new JFrame();
         jFrameInterfaz.setSize(400, 400);
         //jFrameInterfaz.pack();
         jFrameInterfaz.setLocationRelativeTo(null);
         jFrameInterfaz.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jFrameInterfaz.setResizable(false);
         jFrameInterfaz.setTitle("Actividad con JFrame");
+
 
         //creacion de lista de elementos (JList)
         String[] opciones = {"Rosado", "Cyan", "Gris", "Verde", "Amarillo"};
@@ -33,8 +35,19 @@ public class JFrameActividad {
             //comprobar si aun se esta ajustando la seleccion
             if (!e.getValueIsAdjusting()) {
                 String seleccion = lista.getSelectedValue();
+                cambiarColorDeFondo(seleccion);
             }
         });
         jFrameInterfaz.setVisible(true);
+    }
+
+    private static void cambiarColorDeFondo(String seleccion) {
+        switch (seleccion) {
+            case "Rosado" -> jFrameInterfaz.getContentPane().setBackground(Color.PINK);
+            case "Cyan" -> jFrameInterfaz.getContentPane().setBackground(Color.CYAN);
+            case "Gris" -> jFrameInterfaz.getContentPane().setBackground(Color.GRAY);
+            case "Verde" -> jFrameInterfaz.getContentPane().setBackground(Color.GREEN);
+            case "Amarillo" -> jFrameInterfaz.getContentPane().setBackground(Color.YELLOW);
+        }
     }
 }
